4d88df08adcb07665195dbfb5f0c3a900df6a611
yuyatabata <tabayu5@gmail.com>
2022-08-01 08:42:54 +0900

    ロジックの修正

--- lib/tasks/push.rake
+++ lib/tasks/push.rake
@@ -4,18 +4,15 @@ namespace :push_line do
   desc "明日のゴミ出し"
   task push_line_message_trash: :environment do
     trash_day = TrashDay.new
-    users = [ { uid: 1, name: ENV["NAME_OF_PERSON_IN_CHARGE_1"] }, { uid: 2, name: ENV["NAME_OF_PERSON_IN_CHARGE_2"] }]
 
     client = Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
-    users.each do |user|
-      message = {
-        type: 'text',
-        text: trash_day.text(user['name'])
-      }  
-      client.push_message(ENV["LINE_ID_OF_GROUP"], message)
-    end
+    message = {
+      type: 'text',
+      text: trash_day.text
+    }  
+    client.push_message(ENV["LINE_ID_OF_GROUP"], message)
   end
 end
--- lib/tasks/trash_day.rb
+++ lib/tasks/trash_day.rb
@@ -1,18 +1,21 @@
 require_relative 'date_helper.rb'
 
 class TrashDay
-  def text(user_name)
+  def text()
+    user_1 = ENV["NAME_OF_USER_IN_CHARGE_1"]
+    user_2 = ENV["NAME_OF_USER_IN_CHARGE_2"]
+
     date = Date.today
 
     case date.strftime('%a')
       when "Mon"
-        "#{user_name}! 明日は可燃ゴミの日やでぇ！今のうちにまとめときやあ！"
+        "#{user_1}！明日は可燃ゴミの日やでぇ！今のうちにまとめときや〜！"
       when "Tue"
-        "#{user_name}! 明日はプラスチックと古紙・布の日やでぇ！今のうちにまとめときやあ！"
+        "#{user_2}！明日はプラスチックと古紙・布の日やでぇ！今のうちにまとめときや〜！"
       when "Thu"
-        "#{user_name}! 明日は可燃ゴミの日やでぇ！今のうちにまとめときやあ！"
+        "#{user_2}！明日は可燃ゴミの日やでぇ！今のうちにまとめときや〜！"
       when "Sun"
-        "#{user_name}! 明日は#{non_burnable_text}の日やでぇ！今のうちにまとめときやあ！"
+        "#{user_1}！明日は#{non_burnable_text}の日やでぇ！今のうちにまとめときや〜！"
       else
         ""
       end
